32a0a550f389607590a842136bfe05208d5cdb19
Adamkevicius <imatveyadam@gmail.com>
2025-10-06 11:33:58 +0300

    Initial commit

---
+++ src/main/java/com/example/userauthenticationapi/UserAuthenticationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.userauthenticationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserAuthenticationApiApplication.class, args);
+    }
+
+}
